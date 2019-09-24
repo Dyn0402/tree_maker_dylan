77b0ffe3038fe9ba92ee0b249c15a9dac8d1bb4b
Dyn0402 <Dyn0402@hotmail.com>
2019-09-24 15:22:39 -0700

    Load_Macro just uses ProcessLine commands.

--- StRoot/MyAnalysisMaker/Load_Macro.cpp
+++ StRoot/MyAnalysisMaker/Load_Macro.cpp
@@ -1,31 +1,12 @@
 //Dylan Edited 06/25/19
 
-#include <iostream>
-#include <vector>
-
-#include "TFile.h"
-#include "TTree.h"
-#include "TRandom3.h"
-#include "TBranch.h"
 #include "TROOT.h"
-#include "TSystem.h"
-
-#include "Track.h"
-#include "DEvent.h"
-
-
-using namespace std;
-
-int Class_Test();
 
 
 int Load_Macro() {
 	gROOT->ProcessLine(".L DEvent.cpp+");
 	gROOT->ProcessLine(".L Track.cpp+");
-	gROOT->ProcessLine(".L Class_Test.cpp");
-	gSystem->Load("DEvent_cpp.so");
-	gSystem->Load("Track_cpp.so");
-//	vec_tree_test();
-	Class_Test();
+	gROOT->ProcessLine(".L Class_Test.cpp+");
+	gROOT->ProcessLine("Class_Test()");
 	return(0);
 }
