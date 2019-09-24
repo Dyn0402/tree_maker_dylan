979035e7837cd13fa78610babe9be22fcc4ebd2e
Dyn0402 <Dyn0402@hotmail.com>
2019-09-24 16:49:00 -0700

    Added explict references to std namespace in Load_Macro

--- StRoot/MyAnalysisMaker/Load_Macro.cpp
+++ StRoot/MyAnalysisMaker/Load_Macro.cpp
@@ -5,14 +5,12 @@
 #include "TROOT.h"
 #include "TString.h"
 
-using namespace std;
-
 
 int Load_Macro(TString InputFileList, TString OutputDir, int energy) {
 	gROOT->ProcessLine(".L DEvent.cpp+");
 	gROOT->ProcessLine(".L Track.cpp+");
 	gROOT->ProcessLine(".L Class_Test.cpp+");
-	string line = ".x Class_Test(\"" + InputFileList + "\", \"" + OutputDir + "\", " + to_string(energy) + ")";
+	std::string line = ".x Class_Test(\"" + InputFileList + "\", \"" + OutputDir + "\", " + std::to_string(energy) + ")";
 	gROOT->ProcessLine(line.data());
 	return(0);
 }
