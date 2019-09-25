492642588fd5426130c53518ce5ebe769197da51
Dyn0402 <Dyn0402@hotmail.com>
2019-09-24 18:05:18 -0700

    Added Load macro for running readMuDst

---
+++ Load.cpp
@@ -0,0 +1,21 @@
+//Dylan Edited 06/25/19
+
+#include <string>
+#include <sstream>
+
+#include "TROOT.h"
+#include "TString.h"
+
+
+int Load_Macro(TString InputFileList, TString OutputDir, int energy) {
+	gROOT->ProcessLine(".L ./StRoot/MyAnalysisMaker/Event.cpp+");
+	gROOT->ProcessLine(".L ./StRoot/MyAnalysisMaker/Track.cpp+");
+	gROOT->ProcessLine(".L readMuDst.C+");
+	ostringstream energy_str;
+	energy_str << energy;
+	std::string line = "readMuDst(\"" + InputFileList + "\", \"" + OutputDir + "\", ";
+	line += energy_str.str() + ")";
+	cout << line << endl;
+	gROOT->ProcessLine(line.data());
+	return(0);
+}
